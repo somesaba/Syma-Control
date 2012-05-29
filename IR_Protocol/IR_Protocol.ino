bec9b9dab0776b419da76e089988efb767ab1ac9
Saba Khalilnaji <somesaba@gmail.com>
2012-05-28 20:14:23 -0700

    control works

--- IR_Protocol/IR_Protocol.ino
+++ IR_Protocol/IR_Protocol.ino
@@ -1,22 +1,45 @@
-const int irLedPin = 12;
+const int irLedPin = 3;
+const int xpin = A5;
+const int ypin = A3;
+const int throttlePin = A7;
+
 unsigned long lastSendTimeMs = 0;
-int yaw = 0;
-int pitch = 0;
+int yaw = 63;
+int pitch = 63;
 int throttle = 127;
-int yawCorrection = 0;
+int yawCorrection = 63;
+
+int prevX = 0;
+int prevY = 0;
+float alpha = .3;
 
 void setup() {
+  analogReference(EXTERNAL);
   //Serial.begin(115200);
   pinMode(irLedPin, OUTPUT);
+  prevX = analogRead(xpin);
+  delay(100);
+  prevY = analogRead(ypin);
+
 }
 
 void loop() {
-  if(millis() - lastSendTimeMs >= 180) {
-    lastSendTimeMs = millis();
-    sendCommands();
-    //unsigned long diff = millis() - lastSendTimeMs;
-    //Serial.println(diff);
+
+  while(millis() - lastSendTimeMs < 115) {
   }
+  lastSendTimeMs = millis();
+  sendCommands();
+  //unsigned long now = millis();
+  throttle = analogRead(throttlePin) / 8;
+  delay(20);
+  int x = (alpha*analogRead(xpin) + (1 - alpha)*prevX);
+  prevX = x;
+  pitch = 127 - (x - 454);
+  delay(20);
+  int y = (alpha*analogRead(ypin) + (1 - alpha)*prevY);
+  prevY = y;
+  yaw = 127 - (y - 454);
+  delay(20);
 }
 
 
@@ -24,25 +47,14 @@ void sendCommands() {
   sendStartSymbol();
   sendControl(yaw);
   sendControl(pitch);
-  sendThrottle();
+  sendControl(throttle);
   sendControl(yawCorrection);
   sendEndSymbol();  
 }
 
-void sendThrottle() {
-  sendOne();
-  for(int i = 0; i < 7; i++) {
-    if(bitRead(throttle, i) == 0) {
-      sendZero();
-    } else {
-      sendOne();
-    }
-  }
-}
-
 void sendControl(int val) {
   sendZero();
-  for(int i = 0; i < 7; i++) {
+  for(int i = 6; i >= 0; i--) {
     if(bitRead(val, i) == 0) {
       sendZero();
     } else {
@@ -56,33 +68,36 @@ void sendControl(int val) {
 void sendStartSymbol() {
    for(int i = 0; i < 77; i++) {
     digitalWrite(irLedPin, HIGH);
-    delayMicroseconds(13);
+    delayMicroseconds(6);
     digitalWrite(irLedPin, LOW);
-    delayMicroseconds(13);
+    delayMicroseconds(6);
   }
    delayMicroseconds(1998);
 }
 
 void sendEndSymbol() {
-  setHigh300Ms();
+  setHigh300us();
 }
 
 void sendZero() {
-  setHigh300Ms();
-  delayMicroseconds(288);
+  setHigh300us();
+  delayMicroseconds(300);
 }
 
 void sendOne() {
-  setHigh300Ms();
-  delayMicroseconds(688);
+  setHigh300us();
+  delayMicroseconds(695);
 }
 
-void setHigh300Ms() {
-  for(int i = 0; i < 12; i++) {
+void setHigh300us() {
+  for(int i = 0; i < 11; i++) {
     digitalWrite(irLedPin, HIGH);
-    delayMicroseconds(13);
+    delayMicroseconds(6);
     digitalWrite(irLedPin, LOW);
-    delayMicroseconds(13);
-  }
+    delayMicroseconds(6);
+  } 
+  digitalWrite(irLedPin, HIGH);
+  delayMicroseconds(6);
+  digitalWrite(irLedPin, LOW);
 }
   
